50571aaf191d39c079e354e5ba85e7ac84c8a750
sebastien-d-me <81520399+sebastien-d-me@users.noreply.github.com>
2024-02-21 16:53:43 +0100

    ✨ | Add Countdown

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -29,6 +29,25 @@ const countdownHours = document.querySelector(".countdown__value--hours");
 const countdownMinutes = document.querySelector(".countdown__value--minutes");
 const countdownSecondes = document.querySelector(".countdown__value--seconds");
 
+const countdown = setInterval(function() {
+    const currentDate = new Date().getTime();
+    const eventDate = new Date("07/01/2024").getTime();
+    const diffDate = eventDate - currentDate;
+    const countdownDaysValue = Math.floor(diffDate / (1000 * 60 * 60 * 24));
+    const countdownHoursValue = Math.floor((diffDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const countdownMinutesValue = Math.floor((diffDate % (1000 * 60 * 60)) / (1000 * 60));
+    const countdownSecondesValue = Math.floor((diffDate % (1000 * 60)) / 1000);
+
+    countdownDays.textContent = countdownDaysValue;
+    countdownHours.textContent = countdownHoursValue;
+    countdownMinutes.textContent = countdownMinutesValue;
+    countdownSecondes.textContent = countdownSecondesValue;
+
+    if (diffDate < 0) {
+        clearInterval(countdown);
+    }
+}, 1000);
+
 
 
 /** Footer **/
