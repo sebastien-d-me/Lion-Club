b6ffbd29f0c490d62fbaaf4d5a030113b49ed644
sebastien-d-me <81520399+sebastien-d-me@users.noreply.github.com>
2024-02-21 11:46:59 +0100

    ✨ | Header continue

---
+++ assets/scripts/script.js
@@ -0,0 +1,24 @@
+const htmlTag = document.querySelector("html");
+
+/** Menu **/
+const openMenu = document.querySelector(".nav__mobile--open");
+const closeMenu = document.querySelector(".nav__mobile--close");
+const navList = document.querySelector(".nav__list");
+const navLinks = document.querySelectorAll(".nav__link");
+const mainBlock = document.querySelector(".main__block");
+
+function mobileMenu() {
+    htmlTag.classList.toggle("overflow--hide");
+    openMenu.classList.toggle("hide");
+    navList.classList.toggle("show");
+    mainBlock.classList.toggle("blur");
+}
+
+openMenu.addEventListener("click", mobileMenu);
+closeMenu.addEventListener("click", mobileMenu);
+
+navLinks.forEach(navLink => {
+    if (window.innerWidth < 1150) {
+        navLink.addEventListener("click", mobileMenu);
+    }
+});
